2af7630af78963c48b0daad270b25ec230ca25ee
ollsamllo <sam.puriwat@gmail.com>
2017-11-07 12:02:34 +0700

    Before Lunch

--- app/src/main/java/app/rmutsv/sampuriwat/rmutsvservice/fragment/RegisterFragment.java
+++ app/src/main/java/app/rmutsv/sampuriwat/rmutsvservice/fragment/RegisterFragment.java
@@ -1,7 +1,5 @@
 package app.rmutsv.sampuriwat.rmutsvservice.fragment;
-/**
- * Created by samPuriwat on 6/11/2560.
- */
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,18 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.zip.Inflater;
-
 import app.rmutsv.sampuriwat.rmutsvservice.R;
 
+/**
+ * Created by samPuriwat on 7/11/2560.
+ */
 
-
-public class MainFragment extends Fragment {
+public class RegisterFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main,container,false);
-
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
         return view;
+
     }
 }// main class
