b5285907012044be633930b3480cbb2f547ef0af
ollsamllo <sam.puriwat@gmail.com>
2017-11-07 13:58:16 +0700

    On Click Listener

--- app/src/main/java/app/rmutsv/sampuriwat/rmutsvservice/fragment/MainFragment.java
+++ app/src/main/java/app/rmutsv/sampuriwat/rmutsvservice/fragment/MainFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.zip.Inflater;
 
@@ -16,6 +17,33 @@ import app.rmutsv.sampuriwat.rmutsvservice.R;
 
 
 public class MainFragment extends Fragment {
+//    Manager worked after onCreateView Success
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        Register Controller
+        registerController();
+
+
+    }// main Method
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentFragmentMain, new RegisterFragment())
+                        .addToBackStack(null)  //stack Fragment
+                        .commit();
+
+
+            }// onclick
+        });
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
