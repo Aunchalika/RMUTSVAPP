cabe761f7a8dc10df5e6a83770f337397482add8
ollsamllo <sam.puriwat@gmail.com>
2017-11-07 10:34:18 +0700

    update android course 7/11/2017 9.23 AM

--- app/src/main/java/app/rmutsv/sampuriwat/rmutsvservice/MainActivity.java
+++ app/src/main/java/app/rmutsv/sampuriwat/rmutsvservice/MainActivity.java
@@ -3,11 +3,20 @@ package app.rmutsv.sampuriwat.rmutsvservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import app.rmutsv.sampuriwat.rmutsvservice.fragment.MainFragment;
 
+public class MainActivity extends AppCompatActivity {
+    // onCreate is a main method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);  // save process of program that still running
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain, new MainFragment()).commit();
+        }
+
+    }// main method
+}// main class
